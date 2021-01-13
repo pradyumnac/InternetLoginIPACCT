e673302b33159627df4411232dd84b9e66b2a1ac
pradyumna <pady92@gmail.com>
2021-01-13 17:57:56 +0530

    added auto crontab generation in setup.sh

--- setup.sh
+++ setup.sh
@@ -7,3 +7,5 @@
 # setup pasword in .env file from sample.env
 
 # add entry in crontab
+(crontab -l | grep '/scripts/ipacct/run.sh') || { crontab -l; echo '0 * * * * ./scripts/ipacct/run.sh'; } | crontab -
+# echo "0 * * * * ./scripts/ipacct/run.sh"
